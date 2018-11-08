5ae2110c71f19abdd4bd234527d7105e3e26b831
KSH <rhzhzh3@naver.com>
2018-11-08 16:13:42 +0900

    KING

--- Crizel/src/main/webapp/js/crizel.js
+++ Crizel/src/main/webapp/js/crizel.js
@@ -1,5 +1,5 @@
 function goLink(){
-	var array = ["/list.do?mode=nyaa", "/torrent.do", "/onejav.do", "https://5siri.com/xe/index.php?mid=manko"
+	var array = ["/list.do?mode=nyaa", "/onejav.do", "https://5siri.com/xe/index.php?mid=manko"
 		, "/girls.do", "/comic.do"];
 	
 	for(var i=0; i<array.length; i++){
