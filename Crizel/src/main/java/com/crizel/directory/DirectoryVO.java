4419667f3f9513e097c68560fd29fbe1bdef5960
Crizel <rhzhzh3@naver.com>
2022-05-01 12:36:07 +0900

    Delete Crizel directory

--- Crizel/src/main/java/com/crizel/directory/DirectoryVO.java
+++
@@ -1,8 +0,0 @@
-package com.crizel.directory;
-
-import org.springframework.stereotype.Repository;
-
-@Repository("DirectoryVO")
-public class DirectoryVO {
-	
-}
--- Crizel/src/main/java/com/crizel/nyaa/NyaaVO.java
+++
@@ -1,8 +0,0 @@
-package com.crizel.nyaa;
-
-import org.springframework.stereotype.Repository;
-
-@Repository("nyaaVO")
-public class NyaaVO {
-	
-}
